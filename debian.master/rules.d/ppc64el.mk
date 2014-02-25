34097a0af43fb07b57622f99c090cee99aced023
Hayden Bickerton <hayden@hayden-4285dx>
2014-02-25 11:56:29 -0600

    Kernel Optimized for HP DV7 4285DX

---
+++ debian.master/rules.d/ppc64el.mk
@@ -0,0 +1,13 @@
+human_arch	= PowerPC 64el
+build_arch	= powerpc
+header_arch	= $(build_arch)
+defconfig	= pseries_le_defconfig
+flavours	= generic
+build_image	= vmlinux.strip
+kernel_file     = arch/powerpc/boot/vmlinux.strip
+install_file	= vmlinux
+no_dumpfile	= true
+loader		= yaboot
+do_extras_package = true
+
+#do_flavour_image_package = false
